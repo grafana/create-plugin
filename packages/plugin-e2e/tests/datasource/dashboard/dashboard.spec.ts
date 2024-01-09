6bda019ddfc0a6d6711d45abec0c48946af9e618
Erik Sundell <erik.sundell87@gmail.com>
2024-01-09 20:50:38 +0100

    add dashboard tests

--- packages/plugin-e2e/tests/datasource/dashboard/dashboard.spec.ts
+++ packages/plugin-e2e/tests/datasource/dashboard/dashboard.spec.ts
@@ -1,30 +1,27 @@
 import { test, expect, DashboardPage } from '../../../src';
 import { Dashboard } from '../../../src/types';
 
-// test('dashboard', async ({ page, selectors, grafanaVersion, readProvision, request }) => {
-//   const dashboard = await readProvision<Dashboard>({ filePath: 'dashboards/google-sheets.json' });
-//   const dashboardPage = new DashboardPage({ page, selectors, grafanaVersion, request }, { uid: '-CTtXWXMks' });
-//   // const queryResponses = await dashboardPage.goto();
-//   await dashboardPage.goto();
-//   const queryResponses = await dashboardPage.refreshDashboard();
-//   expect(queryResponses).allToBeOK();
-// });
-
-// test('dashboard2', async ({ page, selectors, grafanaVersion, readProvision, request }) => {
-//   const dashboard = await readProvision<Dashboard>({ filePath: 'dashboards/google-sheets.json' });
-//   const dashboardPage = new DashboardPage({ page, selectors, grafanaVersion, request }, { uid: 'x9jSO5c7z' });
-//   const queryResponses = await dashboardPage.goto();
-//   expect(queryResponses).allToBeOK();
-// });
-
-test('dashboard3', async ({ page, selectors, grafanaVersion, readProvision, request }) => {
-  const dashboardPage = new DashboardPage({ page, selectors, grafanaVersion, request }, { uid: '7M8fNhz7k' });
-  const res = await dashboardPage.gotoAndWaitForQueryResponses();
-  expect(res).allToBeOK();
+test('two successful queries should be executed when loading the Google Sheets dashboard', async ({
+  page,
+  selectors,
+  grafanaVersion,
+  request,
+  readProvision,
+}) => {
+  const dashboard = await readProvision<Dashboard>({ filePath: 'dashboards/google-sheets.json' });
+  const dashboardPage = new DashboardPage({ page, selectors, grafanaVersion, request }, { uid: dashboard.uid });
+  expect(await dashboardPage.gotoAndWaitForQueries()).toHaveNSuccessfulResponses(2);
 });
 
-test('dashboard4', async ({ page, selectors, grafanaVersion, readProvision, request }) => {
-  const dashboardPage = new DashboardPage({ page, selectors, grafanaVersion, request }, { uid: '7M8fNhz7k' });
+test('all queries should be successful when loading the ', async ({
+  page,
+  selectors,
+  grafanaVersion,
+  request,
+  readProvision,
+}) => {
+  const dashboard = await readProvision<Dashboard>({ filePath: 'dashboards/redshift.json' });
+  const dashboardPage = new DashboardPage({ page, selectors, grafanaVersion, request }, { uid: dashboard.uid });
   await dashboardPage.goto();
-  expect(await dashboardPage.refreshDashboard()).allToBeOK();
+  expect(await dashboardPage.refreshDashboard()).toBeOK();
 });
