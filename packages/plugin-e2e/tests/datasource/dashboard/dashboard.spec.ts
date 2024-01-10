c124e237f2b6913d340292424115c59aabf3c049
Erik Sundell <erik.sundell87@gmail.com>
2024-01-10 15:41:15 +0100

    add some more test dashboards

--- packages/plugin-e2e/tests/datasource/dashboard/dashboard.spec.ts
+++ packages/plugin-e2e/tests/datasource/dashboard/dashboard.spec.ts
@@ -25,3 +25,19 @@ test('all queries should be successful when loading the ', async ({
   await dashboardPage.goto();
   expect(await dashboardPage.refreshDashboard()).toBeOK();
 });
+
+const dashboards: Array<{ name?: string; uid: string; successful: number; total: number }> = [
+  { uid: 'x9jSO5c7z', name: 'Cloudwatch', successful: 15, total: 17 },
+  { uid: 'GbeyiI3Gz', name: 'Azure Monitor Erik', successful: 5, total: 5 },
+  { uid: 'vmie2cmWz', name: 'Bar gauge data', successful: 6, total: 6 },
+];
+
+for (const d of dashboards) {
+  test('dashboards' + d.uid, async ({ page, selectors, grafanaVersion, request }) => {
+    const dashboardPage = new DashboardPage({ page, selectors, grafanaVersion, request }, { uid: d.uid });
+    const res = await dashboardPage.gotoAndWaitForQueries();
+    expect.soft(res.length).toEqual(d.total);
+    expect(res).toHaveNSuccessfulResponses(d.successful);
+  });
+  // You can also do it with test.describe() or with multiple tests as long the test name is unique.
+}
